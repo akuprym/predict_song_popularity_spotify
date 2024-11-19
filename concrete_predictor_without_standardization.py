37ef7d8abd48f952b63124ed452c539922e52417
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-11-20 00:10:46 +0300

    Initial commit

---
+++ concrete_predictor_without_standardization.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import layers
+
+sns.set_theme(style="whitegrid")
+
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('animation', html='html5')
+
+concrete = pd.read_csv('concrete.csv')
+df = concrete.copy()
+
+df_train = df.sample(frac=0.7, random_state=0)
+df_valid = df.drop(df_train.index)
+
+X_train = df_train.drop('CompressiveStrength', axis=1)
+X_valid = df_valid.drop('CompressiveStrength', axis=1)
+y_train = df_train['CompressiveStrength']
+y_valid = df_valid['CompressiveStrength']
+
+input_shape = [X_train.shape[1]]
+
+model = keras.Sequential([
+    layers.Input(shape=input_shape),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(1),
+])
+
+model.compile(
+    optimizer='sgd',
+    loss='mae',
+    metrics=['mae'],
+)
+
+EPOCHS = 100
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=64,
+    epochs=EPOCHS,
+    verbose=0,
+)
+
+history_df = pd.DataFrame(history.history)
+history_df.loc[0:, ['loss', 'val_loss']].plot()
+print(("Minimum Validation Loss: {:0.4f}").format(history_df['val_loss'].min()))
